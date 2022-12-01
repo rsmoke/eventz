19269442f68978ab8a8ced50d8434bcc3fe8725b
Rick Smoke <rsmoke@umich.edu>
2022-12-01 08:27:12 -0500

    Initial commit

---
+++ app/helpers/events_helper.rb
@@ -0,0 +1,23 @@
+module EventsHelper
+
+  def main_image(event)
+    if event.main_image.attached?
+      image_tag event.main_image
+    else
+      image_tag "placeholder.png"
+    end
+  end
+
+  def price(event)
+    if event.free?
+      "Free"
+    else
+      number_to_currency(event.price, precision: 0)
+    end
+  end
+
+  def day_and_time(event)
+    event.starts_at.strftime("%B %d at %I:%M %P")
+  end
+
+end
---
+++ app/models/event.rb
@@ -0,0 +1,64 @@
+class Event < ApplicationRecord
+
+  before_save :set_slug
+
+  has_many :registrations, dependent: :destroy
+  has_many :likes, dependent: :destroy
+  has_many :likers, through: :likes, source: :user
+  has_many :categorizations, dependent: :destroy
+  has_many :categories, through: :categorizations
+
+  has_one_attached :main_image
+
+  validates :name, presence: true, uniqueness: true
+  validates :location, presence: true
+
+  validates :description, length: { minimum: 25 }
+
+  validates :price, numericality: { greater_than_or_equal_to: 0 }
+
+  validates :capacity, numericality:
+                       { only_integer: true, greater_than: 0 }
+
+  validate :acceptable_image
+
+  scope :past, -> { where("starts_at < ?", Time.now).order("starts_at") }
+  scope :upcoming, -> { where("starts_at > ?", Time.now).order("starts_at") }
+  scope :free, -> { upcoming.where(price: 0.0).order(:name) }
+  scope :recent, ->(max=3) { past.limit(max) }
+
+  def free?
+    price.blank? || price.zero?
+  end
+
+  def sold_out?
+    (capacity - registrations.size).zero?
+  end
+
+  def to_param
+    slug
+  end
+
+private
+
+  def acceptable_image
+    return unless main_image.attached?
+
+    unless main_image.blob.byte_size <= 1.megabyte
+      errors.add(:main_image, "is too big")
+    end
+
+    acceptable_types = ["image/jpeg", "image/png"]
+    unless acceptable_types.include?(main_image.content_type)
+      errors.add(:main_image, "must be a JPEG or PNG")
+    end
+  end
+
+
+
+
+  def set_slug
+    self.slug = name.parameterize
+  end
+
+end
